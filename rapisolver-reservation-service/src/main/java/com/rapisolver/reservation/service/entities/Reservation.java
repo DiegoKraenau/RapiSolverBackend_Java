7ce07cb7085850ac62272338b5e4996471614d39
nivra <nivraleak@outlook.com>
2021-06-11 12:20:20 -0500

    Agreegando atributos de Reservation y carpeta util

--- rapisolver-reservation-service/src/main/java/com/rapisolver/reservation/service/entities/Reservation.java
+++ rapisolver-reservation-service/src/main/java/com/rapisolver/reservation/service/entities/Reservation.java
@@ -1,12 +1,13 @@
 package com.rapisolver.reservation.service.entities;
 
+import com.rapisolver.reservation.service.enums.StatusOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,5 +17,24 @@ import javax.persistence.Table;
 @Entity
 public class Reservation {
 
-    /*  TODO = PONER LAS VARIABLES CORREPONDIENTES A LA CLASE RESERVATIONS*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
+
+    private Long userId;
+
+    private Long supplierAttentionId;
+
+    @Column(nullable = false)
+    private Date dateRequested;
+
+    @Column(length = 100, nullable = false)
+    private String comment;
+
+    @Column
+    private StatusOrder status;
 }
