f8a80ab36660873a0d8b4349283dee492c439c16
Andre Gonzales <andreg-16@hotmail.com>
2021-06-10 22:46:39 -0500

    UserService Endpoint Implemented

---
+++ rapisolver-service-service/src/main/java/com/rapisolver/service/controllers/UserServiceController.java
@@ -0,0 +1,35 @@
+package com.rapisolver.service.controllers;
+
+import com.rapisolver.service.dtos.CreateUserServiceDTO;
+import com.rapisolver.service.dtos.UserServiceDTO;
+import com.rapisolver.service.services.UserServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "users-services")
+public class UserServiceController {
+
+    @Autowired
+    private UserServiceService service;
+
+    @PostMapping
+    public ResponseEntity<UserServiceDTO> create(@RequestBody CreateUserServiceDTO c) throws RuntimeException {
+        return new ResponseEntity<>(service.create(c), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserServiceDTO>> getAll() throws RuntimeException {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserServiceDTO> getById(@PathVariable Long id) throws RuntimeException {
+        return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
+    }
+
+}
--- rapisolver-service-service/src/main/java/com/rapisolver/service/services/impl/UserServiceServiceImpl.java
+++ rapisolver-service-service/src/main/java/com/rapisolver/service/services/impl/UserServiceServiceImpl.java
@@ -2,31 +2,49 @@ package com.rapisolver.service.services.impl;
 
 import com.rapisolver.service.dtos.CreateUserServiceDTO;
 import com.rapisolver.service.dtos.UserServiceDTO;
+import com.rapisolver.service.entities.UserService;
 import com.rapisolver.service.repositories.UserServiceRepository;
 import com.rapisolver.service.services.UserServiceService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceServiceImpl implements UserServiceService {
+
+    @Autowired
+    private ModelMapper mapper;
 
     @Autowired
     private UserServiceRepository repository;
 
     @Override
     public UserServiceDTO create(CreateUserServiceDTO c) throws RuntimeException {
-        return null;
+
+        try {
+            UserService userService = UserService.builder()
+                    .detail(c.getDetail())
+                    .price(c.getPrice())
+                    .build();
+            userService = repository.save(userService);
+            return mapper.map(userService, UserServiceDTO.class);
+        } catch (Exception e) {
+            throw new RuntimeException("CREATE_USER_SERVICE_ERROR");
+        }
     }
 
     @Override
     public List<UserServiceDTO> getAll() throws RuntimeException {
-        return null;
+        List<UserService> userServices = repository.findAll();
+        return userServices.stream().map(us -> mapper.map(us, UserServiceDTO.class)).collect(Collectors.toList());
     }
 
     @Override
     public UserServiceDTO getById(Long id) throws RuntimeException {
-        return null;
+        UserService userServiceDB = repository.findById(id).orElseThrow(() -> new RuntimeException("USER_SERVICE_NOT_FOUND"));
+        return mapper.map(userServiceDB, UserServiceDTO.class);
     }
 }
