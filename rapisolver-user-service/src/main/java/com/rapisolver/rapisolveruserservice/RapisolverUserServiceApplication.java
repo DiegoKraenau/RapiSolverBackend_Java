c0e8ab40626a4388c48c733608a80d92ffdbb0b3
Jesus Duran Huancas <jesusgonzaloduran@gmail.com>
2021-06-11 16:31:26 -0500

    Project Creation

---
+++ rapisolver-user-service/src/main/java/com/rapisolver/rapisolveruserservice/RapisolverUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rapisolver.rapisolveruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapisolverUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapisolverUserServiceApplication.class, args);
+    }
+
+}
