711c6c55cd985c9af85309f533bb39b01c1d1cab
Andre Gonzales <andreg-16@hotmail.com>
2021-06-10 20:52:44 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RapiSolverBackend_Java
---
+++ rapisolver-service-service/src/test/java/com/rapisolver/service/RapisolverServiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapisolver.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapisolverServiceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
