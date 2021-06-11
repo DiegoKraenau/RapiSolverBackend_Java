62b14de378f77bddf9dd135819cd83bf36444b23
nivra <nivraleak@outlook.com>
2021-06-11 00:48:12 -0500

    Agregando proyecto

---
+++ rapisolver-reservation-service/src/main/java/com/rapisolver/reservation/service/RapisolverReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rapisolver.reservation.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapisolverReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapisolverReservationServiceApplication.class, args);
+    }
+
+}
