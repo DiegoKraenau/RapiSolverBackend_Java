b3e6bbd43fab3277c470dcacf4a38f74e64c3bad
Jesus Duran Huancas <jesusgonzaloduran@gmail.com>
2021-06-11 20:01:22 -0500

    Fix h2-error

--- rapisolver-user-service/src/main/resources/data.sql
+++ rapisolver-user-service/src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO roles(id, can_publish, name )
-VALUES (1, false , 'CUSTOMER');
+VALUES ('1', false , 'CUSTOMER');
 
+INSERT INTO roles(id, can_publish, name)
+VALUES ('2', false , 'SUPPLIER');
